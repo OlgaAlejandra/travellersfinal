3bd991b992319d08316aee84b5052e141493d4e2
Alejandra Ascue <olgaaa2003@gmail.com>
2022-05-24 18:37:53 -0500

    Inicio de Repositorio

---
+++ src/main/java/pe/edu/upc/ejemplo/TravellersfinalApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.ejemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravellersfinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravellersfinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/upc/ejemplo/TravellersfinalApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.ejemplo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravellersfinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
